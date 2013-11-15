77cd26e75702e4aba2e7e5e3542815e32b401291
Ian Pye <ianpye@gmail.com>
2013-11-14 19:43:08 -0800

    Now with some basic getters and setters

--- whitedb/whitedb.go
+++ whitedb/whitedb.go
@@ -15,6 +15,24 @@ import (
 	"unsafe"
 )
 
+// Export some record types to Go.
+const (
+	NULLTYPE       = C.WG_NULLTYPE
+	RECORDTYPE     = C.WG_RECORDTYPE
+	INTTYPE        = C.WG_INTTYPE
+	DOUBLETYPE     = C.WG_DOUBLETYPE
+	STRTYPE        = C.WG_STRTYPE
+	XMLLITERALTYPE = C.WG_XMLLITERALTYPE
+	URITYPE        = C.WG_URITYPE
+	BLOBTYPE       = C.WG_BLOBTYPE
+	CHARTYPE       = C.WG_CHARTYPE
+	FIXPOINTTYPE   = C.WG_FIXPOINTTYPE
+	DATETYPE       = C.WG_DATETYPE
+	TIMETYPE       = C.WG_TIMETYPE
+	ANONCONSTTYPE  = C.WG_ANONCONSTTYPE
+	VARTYPE        = C.WG_VARTYPE
+)
+
 // WDBError is used for errors using the whitedb library.  It implements the
 // builtin error interface.
 type WDBError string
@@ -27,6 +45,13 @@ func (err WDBError) Error() string {
 type WhiteDB struct {
 	db unsafe.Pointer
 }
+
+// A record pointer
+type Record struct {
+	rec unsafe.Pointer
+}
+
+//// Work with Databases.
 
 // Attatches to a DB. Creates the db if it does not yet exist.
 func AttachDatabase(address string, size int) (*WhiteDB, error) {
@@ -56,3 +81,131 @@ func (d *WhiteDB) DetatchDatabase() {
 		C.wg_detach_database(d.db)
 	}
 }
+
+//// Generate/Delete/Find Records.
+
+// Generate a record.
+func (d *WhiteDB) CreateRecord(length int64) (*Record, error) {
+	r := &Record{rec: C.wg_create_record(d.db, C.wg_int(length))}
+	if r.rec == nil {
+		return nil, WDBError("Could not create record")
+	}
+	return r, nil
+}
+
+// Same as above, but does not create index.
+func (d *WhiteDB) CreateRawRecord(length int64) (*Record, error) {
+	r := &Record{rec: C.wg_create_raw_record(d.db, C.wg_int(length))}
+	if r.rec == nil {
+		return nil, WDBError("Could not create record")
+	}
+	return r, nil
+}
+
+// Removes the record.
+func (d *WhiteDB) DeleteRecord(r *Record) error {
+	if C.wg_delete_record(d.db, r.rec) != 0 {
+		return WDBError("Could not delete record")
+	}
+	return nil
+}
+
+// Seeks to the first record in a db.
+func (d *WhiteDB) GetFirstRecord() (*Record, error) {
+	r := &Record{rec: C.wg_get_first_record(d.db)}
+	if r.rec == nil {
+		return nil, WDBError("Done With DB")
+	}
+	return r, nil
+}
+
+// Increments to next record in the db.
+func (d *WhiteDB) GetNextRecord(r *Record) (*Record, error) {
+	nr := &Record{rec: C.wg_get_next_record(d.db, r.rec)}
+	if nr.rec == nil {
+		return nil, WDBError("Done With DB")
+	}
+	return nr, nil
+}
+
+//// Get set values in records.
+
+// Sets the already encoded value
+func (r *Record) SetField(d *WhiteDB, number uint16, data int64) error {
+	if C.wg_set_field(d.db, r.rec, C.wg_int(number), C.wg_int(data)) != 0 {
+		return WDBError("Could not set field")
+	}
+	return nil
+}
+
+// Same as above, but errors if the field has been set before.
+func (r *Record) SetNewField(d *WhiteDB, number uint16, data int64) error {
+	if C.wg_set_new_field(d.db, r.rec, C.wg_int(number), C.wg_int(data)) != 0 {
+		return WDBError("Could not set field")
+	}
+	return nil
+}
+
+// Convenience function, does the encoding for you.
+func (r *Record) SetInt64Field(d *WhiteDB, number uint16, data int64) error {
+	if C.wg_set_int_field(d.db, r.rec, C.wg_int(number), C.wg_int(data)) != 0 {
+		return WDBError("Could not set field")
+	}
+	return nil
+}
+
+// Takes Doubles
+func (r *Record) SetFloat64Field(d *WhiteDB, number uint16, data float64) error {
+	if C.wg_set_double_field(d.db, r.rec, C.wg_int(number), C.double(data)) != 0 {
+		return WDBError("Could not set field")
+	}
+	return nil
+}
+
+func (r *Record) SetByteField(d *WhiteDB, number uint16, data []byte) error {
+	if C.wg_set_str_field(d.db, r.rec, C.wg_int(number), (*C.char)(unsafe.Pointer(&data[0]))) != 0 {
+		return WDBError("Could not set field")
+	}
+	return nil
+}
+
+///// Getters
+
+// Returns 0 when there was an error.
+// @TODO -- wrap this in better handling?
+func (r *Record) GetField(d *WhiteDB, number uint16) int64 {
+	return int64(C.wg_get_field(d.db, r.rec, C.wg_int(number)))
+}
+
+func (r *Record) GetFieldType(d *WhiteDB, number uint16) int64 {
+	return int64(C.wg_get_field_type(d.db, r.rec, C.wg_int(number)))
+}
+
+func (r *Record) GetInt64Field(d *WhiteDB, number uint16) (int64, error) {
+	if r.GetFieldType(d, number) != INTTYPE {
+		return 0, WDBError("Not an int valued field")
+	}
+	return int64(C.wg_decode_int(d.db, C.wg_get_field(d.db, r.rec, C.wg_int(number)))), nil
+}
+
+func (r *Record) GetFloat64Field(d *WhiteDB, number uint16) (float64, error) {
+	if r.GetFieldType(d, number) != DOUBLETYPE {
+		return 0, WDBError("Not an double valued field")
+	}
+	return float64(C.wg_decode_double(d.db, C.wg_get_field(d.db, r.rec, C.wg_int(number)))), nil
+}
+
+/**
+func (r *Record) GetByteField(d *WhiteDB, number uint16) ([]byte, error) {
+	if r.GetFieldType(d, number) != STRINGTYPE {
+		return 0, WDBError("Not an string valued field")
+	}
+
+	return int64(C.wg_decode_int(d.db, C.wg_get_field(d.db, r.rec, C.wg_int(number)))), nil
+}
+*/
+
+/**
+
+
+*/
--- whitedb/whitedb_test.go
+++ whitedb/whitedb_test.go
@@ -12,17 +12,16 @@ import (
 func TestOpenClose(t *testing.T) {
 
 	db, err := whitedb.AttachDatabase("1000", 2000000)
-	defer db.DetatchDatabase()
-
 	if err != nil {
 		t.Fatal(err)
 	}
+
+	defer db.DetatchDatabase()
 }
 
 func TestDelete(t *testing.T) {
 
 	db, err := whitedb.AttachDatabase("1000", 2000000)
-
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -30,5 +29,99 @@ func TestDelete(t *testing.T) {
 	db.DetatchDatabase()
 	if err = whitedb.DeleteDatabase("1000"); err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestRecordCreateDelete(t *testing.T) {
+	db, err := whitedb.AttachDatabase("1000", 2000000)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer db.DetatchDatabase()
+	recs := make([]*whitedb.Record, 0, 0)
+
+	if r, err := db.CreateRecord(2); err != nil {
+		t.Fatal(err)
+	} else {
+		recs = append(recs, r)
+	}
+
+	if r, err := db.CreateRawRecord(2); err != nil {
+		t.Fatal(err)
+	} else {
+		recs = append(recs, r)
+	}
+
+	for _, r := range recs {
+		err = db.DeleteRecord(r)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestRecordSet(t *testing.T) {
+	db, err := whitedb.AttachDatabase("1000", 2000000)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.DetatchDatabase()
+
+	rec, err := db.CreateRecord(5)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := rec.SetField(db, 0, 0); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := rec.SetNewField(db, 1, 1); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := rec.SetInt64Field(db, 2, 2); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := rec.SetFloat64Field(db, 3, 3.0); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := rec.SetByteField(db, 4, []byte("Test")); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := rec.SetNewField(db, 1, 1); err == nil {
+		t.Errorf("Set new field faled. Expected an error, got nothing.")
+	}
+}
+
+func TestIntGetSet(t *testing.T) {
+	db, err := whitedb.AttachDatabase("1000", 2000000)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.DetatchDatabase()
+	vals := []int64{10, 3242, 23423423}
+
+	rec, err := db.CreateRecord(int64(len(vals)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for i, v := range vals {
+		if err := rec.SetInt64Field(db, uint16(i), v); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	for i, v := range vals {
+		if vsave, err := rec.GetInt64Field(db, uint16(i)); err != nil {
+			t.Fatal(err)
+		} else if v != vsave {
+			t.Errorf("Execting %d, got %d", v, vsave)
+		}
 	}
 }
